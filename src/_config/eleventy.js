8aec3a5cd606570a97e951665a4f39251607dd82
Greg Ives <gregives774@gmail.com>
2020-12-29 23:30:42 +0000

    Update documentation for new config object

--- src/_config/eleventy.js
+++ src/_config/eleventy.js
@@ -1,11 +1,9 @@
 const markdown = require("./markdown");
 
 module.exports = function (content) {
-  const config = JSON.stringify(
-    this.config,
-    (key, value) => (key === "md" ? undefined : value),
-    2
-  );
+  // Ignore libraryOverrides "md" key
+  const ignore = (key, value) => (key === "md" ? undefined : value);
+  const config = JSON.stringify(this.config, ignore, 2);
   const string = `\`\`\`json\n${config}\n\`\`\``;
   return content.replace("<div data-config></div>", markdown.render(string));
 };
